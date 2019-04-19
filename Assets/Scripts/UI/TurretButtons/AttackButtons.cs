a5d7c489d1dbb72183ba58dd5db947c5ff9640e9
blaj95 <32546217+blaj95@users.noreply.github.com>
2019-04-19 00:59:43 -0400

    still setting up button UI system

---
+++ Assets/Scripts/UI/TurretButtons/AttackButtons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AttackButtons : MonoBehaviour
+{
+    public Dictionary<int, Button> attackButtons;
+
+    public List<GameObject> attackTurrets;
+
+    private int index = 0;
+    private void Start()
+    {
+       // attackButtons = new List<Button>();
+       attackButtons = new Dictionary<int, Button>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Button b = transform.GetChild(i).GetComponent<Button>();
+            attackButtons.Add(index,b);
+            b.onClick.AddListener(Click);
+            index += 1;
+        }
+    }
+
+    public void Click()
+    {
+        
+
+
+    }
+    
+    
+}
