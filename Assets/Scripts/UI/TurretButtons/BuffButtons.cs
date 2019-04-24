8b737798ef9bb2621df94544419d8031b0301b29
blaj95 <32546217+blaj95@users.noreply.github.com>
2019-04-23 22:38:19 -0400

    All button types implemented

--- Assets/Scripts/UI/TurretButtons/BuffButtons.cs
+++ Assets/Scripts/UI/TurretButtons/BuffButtons.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuffButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Dictionary<Button, int> buffButtons;
+
+    public List<GameObject> buffTurrets;
+
+    private int index = 0;
+    private void Start()
     {
-        
+        // attackButtons = new List<Button>();
+        buffButtons = new Dictionary<Button,int>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Button b = transform.GetChild(i).GetComponent<Button>();
+            buffButtons.Add(b,index);
+            b.onClick.AddListener(delegate { Click(b); });
+            index += 1;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Click(Button button)
     {
-        
+
+        if (buffButtons.ContainsKey(button))
+        {
+            int i = buffButtons[button];
+            GameObject g = buffTurrets[i];
+            TurretPlacement.SetTurret(g);
+        }
+
     }
 }
--- Assets/Scripts/UI/TurretButtons/DefenseButtons.cs
+++ Assets/Scripts/UI/TurretButtons/DefenseButtons.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenseButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Dictionary<Button, int> defenseButtons;
+
+    public List<GameObject> defenseTurrets;
+
+    private int index = 0;
+    private void Start()
     {
-        
+        // attackButtons = new List<Button>();
+        defenseButtons = new Dictionary<Button,int>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Button b = transform.GetChild(i).GetComponent<Button>();
+            defenseButtons.Add(b,index);
+            b.onClick.AddListener(delegate { Click(b); });
+            index += 1;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Click(Button button)
     {
-        
+
+        if (defenseButtons.ContainsKey(button))
+        {
+            int i = defenseButtons[button];
+            GameObject g = defenseTurrets[i];
+            TurretPlacement.SetTurret(g);
+        }
+
     }
 }
